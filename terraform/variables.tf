477c910545ef2e2455bdec1f7ad8a1b85f816276
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2025-03-04 12:16:35 +0100

    workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "environment" {
 }
 variable "rg_name" {
   type        = string
-  description = "The resource group name"
+  description = "The resource group name2"
   default     = "rc-codeacademy-example"
 }
 
