e36ab9329b23b44be7e30b9a2dc5b2a2e9a24652
Magnus Kongshem <magnus.kongshem@soprasteria.com>
2025-03-04 13:08:00 +0100

    TF

---
+++ terraform/appservice.tf
@@ -0,0 +1,26 @@
+# Create an Azure App Service Plan
+resource "azurerm_service_plan" "main" {
+  name                = "${var.app_service_plan}-${var.environment}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  os_type             = "Windows"
+  sku_name            = "B2"
+  worker_count        = 1
+}
+
+# Create an Azure App Service
+resource "azurerm_linux_web_app" "main" {
+  name                = "${var.app_service}-${var.environment}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_service_plan.main.location
+  service_plan_id     = azurerm_service_plan.main.id
+  https_only          = true
+
+  site_config {
+    http2_enabled = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
